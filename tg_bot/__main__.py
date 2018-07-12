9599202d6b3061341b21332fdd4a291df7180e87
dark-shadow-007 <34188302+dark-shadow-007@users.noreply.github.com>
2018-07-12 11:26:49 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -54,7 +54,7 @@ DONATE_STRING = """Heya, glad to hear you want to donate!
 It took lots of work for [my creator](t.me/Darkshadow-007) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/TrickyTech) , [Paytm](7024002862)"""
+There are two ways of paying him; [PayPal](paypal.me/TrickyTech) , [Paytm @7024002862]"
 
 IMPORTED = {}
 MIGRATEABLE = []
